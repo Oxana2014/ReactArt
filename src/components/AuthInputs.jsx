d98c309244fbb69eab3929c53723b5d81636fe3b
Oksana Kormilina <keysb@mail.ru>
2024-01-09 17:48:02 +0400

    Add CustomInput as combined styled component

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,45 +1,23 @@
-import { useState } from 'react';
-import {styled} from 'styled-components'
+import { useState } from "react";
+import { styled } from "styled-components";
 
-import Button from './Button.jsx'
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
 const ControlsContainer = styled.div`
-display: flex;
-flex-direction: column;
-gap: 0.5rem;
-margin-bottom: 1.5rem;
-`
-
-const Label = styled.label`
-display: block;
-margin-bottom: 0.5rem;
-font-size: 0.75rem;
-font-weight: 700;
-letter-spacing: 0.1em;
-text-transform: uppercase;
-color: ${(props) => props.$invalid ? '#f87171' : '#6b7280'} ;
-`
-
-const Input = styled.input`
-width: 100%;
-padding: 0.75rem 1rem;
-line-height: 1.5;
-background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'} ;
-color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151' } ;
-border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent' };
-border-radius: 0.25rem;
-box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
-
-
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -50,42 +28,29 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
       {/* <div className="controls"> */}
       <ControlsContainer>
-        <p>
-          <Label
-          $invalid={emailNotValid} //add $ before the name of property to avoid clash with build-in props
-          //  className={`label ${emailNotValid ? 'invalid' : ''}`}
-           >Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            //  className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <Label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</Label>
-          <Input
-            type="password"
-            // style={
-            //   {
-            //     backgroundColor:passwordNotValid ? '#fed2d2' : '#d1d5db'
-            //   }
-            // }
-             className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-        </ControlsContainer>
-      
+        <Input
+          label="Email"
+          invalid={emailNotValid}
+          type="email"
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <Input
+          label="Password"
+          invalid={passwordNotValid}
+          type="password"
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)}
+        />
+    
+      </ControlsContainer>
+
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
