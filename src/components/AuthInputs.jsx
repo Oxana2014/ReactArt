1402ef5c73d03ec0951a7235eb4ca820db438c68
Oksana Kormilina <keysb@mail.ru>
2024-01-09 15:00:33 +0400

    Add dynamic styling with styled-components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -15,16 +15,16 @@ font-size: 0.75rem;
 font-weight: 700;
 letter-spacing: 0.1em;
 text-transform: uppercase;
-color: #6b7280;
+color: ${(props) => props.$invalid ? '#f87171' : '#6b7280'} ;
 `
 
 const Input = styled.input`
 width: 100%;
 padding: 0.75rem 1rem;
 line-height: 1.5;
-background-color: #d1d5db;
-color: #374151;
-border: 1px solid transparent;
+background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'} ;
+color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151' } ;
+border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent' };
 border-radius: 0.25rem;
 box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -54,10 +54,14 @@ export default function AuthInputs() {
       {/* <div className="controls"> */}
       <ControlsContainer>
         <p>
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
+          <Label
+          $invalid={emailNotValid} //add $ before the name of property to avoid clash with build-in props
+          //  className={`label ${emailNotValid ? 'invalid' : ''}`}
+           >Email</Label>
           <Input
             type="email"
-             className={emailNotValid ? 'invalid' : undefined}
+            $invalid={emailNotValid}
+            //  className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
