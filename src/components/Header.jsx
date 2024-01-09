2d3536a69b5c7c66807bb936b85f3528c8478798
Oksana Kormilina <keysb@mail.ru>
2024-01-09 12:47:00 +0400

    Add inline styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,10 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p
+       style={{color: 'red',
+      textAlign: 'left'}}
+       >A community of artists and art-lovers.</p>
     </header>
   );
 }
