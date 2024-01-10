e5542f383f24b386baed2381eff253c5b721bab3
Oksana Kormilina <keysb@mail.ru>
2024-01-10 15:58:46 +0400

    Style second button and input group with Tailwind CSS

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,15 +1,8 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 
 import Button from "./Button.jsx";
 import Input from "./Input.jsx";
 
-const ControlsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -32,9 +25,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      {/* <div className="controls"> */}
-      <ControlsContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="Email"
           invalid={emailNotValid}
@@ -49,10 +41,10 @@ export default function AuthInputs() {
             handleInputChange("password", event.target.value)}
         />
     
-      </ControlsContainer>
+      </div>
 
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
